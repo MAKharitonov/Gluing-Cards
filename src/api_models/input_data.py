22a4922dd1e9e8a5fbdcc255dc6bc424ef738a01
mikhail <kharitonov.mihail@gmail.com>
2023-12-02 10:48:59 +0300

    new

--- src/api_models/input_data.py
+++ src/api_models/input_data.py
@@ -1,5 +1,5 @@
 import pydantic_numpy.typing as pnd
-from src.models.boundary import Boundary
+from src.models.piecewise_linear_function import PiecewiseLinearFunction
 from src.task.task import Task
 from typing import List, Optional
 from src.models.zone import Zone
@@ -11,10 +11,11 @@ class InputModel(BaseModel):
     output_file_name: str = Field(alias="outputFileName")
     days: int
     zones: List[Zone]
-    up_bound_volga_zone: Boundary = Field(alias="upBoundVolgaZone")
-    low_bound_akhtuba_zone: Boundary = Field(alias="lowBoundAkhtubaZone")
-    volga_channels: Boundary = Field(alias="volgaChannels")
-    akhtuba_channels: Boundary = Field(alias="akhtubaChannels")
+    up_bound_volga_zone: PiecewiseLinearFunction = Field(alias="upBoundVolgaZone")
+    low_bound_akhtuba_zone: PiecewiseLinearFunction = Field(alias="lowBoundAkhtubaZone")
+    volga_channels: PiecewiseLinearFunction = Field(alias="volgaChannels")
+    akhtuba_channels: PiecewiseLinearFunction = Field(alias="akhtubaChannels")
+    cell_size: int = Field(alias = "cellSize")
 
 
 
--- src/models/boundary.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Optional
-from src.models.zone import Zone
-from pydantic import BaseModel, Field, conint
-
-class Boundary(BaseModel):
-    x: List[int]
-    y: List[int]
-
-
